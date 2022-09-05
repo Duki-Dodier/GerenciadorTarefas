3e954934601c2822c4ee42b2c77236817a4df294
duki <duki.dodier@gmail.com>
2022-09-05 11:48:26 -0300

    frontEnd gerenciador de tarefas concluido

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>GerenciadorTarefas</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>GerenciadorTarefas</h1>
+        </div>
+        <div class="form">
+            <form action="" method="GET">
+                <label for="task_name">Tarefa:</label>
+                <input type="text" name="task_name" placeholder="Nome da Tarefa">
+                <button type="submit">Cadastrar</button>
+            </form>
+
+        </div>
+        <div class="separador">
+
+        </div>
+        <div class="list-tasks">
+            <ul>
+                <li>tarefa</li>
+                <li>tarefa</li>
+                <li>tarefa</li>
+            </ul>
+        </div>
+        <div class="footer">
+            <p>Desenvolvido por @Kadimus</p>
+        </div>
+    </div>
+
+</body>
+
+</html>
